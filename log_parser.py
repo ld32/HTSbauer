ada35d5d88c73bdb2e7a81e1226d4f0aef08c427
LuisSoares <luis.miguel.mendes.soares@gmail.com>
2015-10-18 20:18:21 -0400

    third commit

--- log_parser.py
+++ log_parser.py
@@ -1,1 +1,13 @@
 __author__ = 'Luis'
+
+class BarCodeSeq:
+    def __init__(self,name,totalreads):
+        self.name=name
+        self.totalreads=totalreads
+        self.sample=None
+        self.unique_reads=None
+        self.dup_reads=None
+        self.unmapped_reads=None
+
+
+log=open('log.out','r')
